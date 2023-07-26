2395071424dc73bf031b3ce98c741cfa9f63ee6f
Jekyun Kim <kkyuni97@naver.com>
2023-07-26 14:24:17 +0900

    기본 목록 페이지, 상세 페이지 추가

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+
+from blog.models import Post
+
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.filter(status=Post.Status.PUBLISHED).all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'blog/post/detail.html', {'post': post})
