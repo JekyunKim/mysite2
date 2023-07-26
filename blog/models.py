e3af972b0513a5cee07e41e9c73b0af8ab338951
Jekyun Kim <kkyuni97@naver.com>
2023-07-26 10:43:38 +0900

    Post 모델 추가 & migration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+    title = models.CharField(max_length=200)
+    # slug = models.SlugField(max_length=200)
+    body = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    # draft = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-published']
+        indexes = [
+            models.Index(fields=['-published'])
+        ]
+
+    def __str__(self):
+        return self.title
