4464df5416e2d7da01440fe92e57c507e57575e0
Jekyun Kim <kkyuni97@naver.com>
2023-07-27 01:15:38 +0900

    paginator 추가

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
@@ -7,10 +8,20 @@ from blog.models import Post
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(status=Post.Status.PUBLISHED).all()
+    page_number = request.GET.get('page', 1)
+    page_list_query = Post.objects.filter(status=Post.Status.PUBLISHED)
+    paginator = Paginator(page_list_query, per_page=3)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
