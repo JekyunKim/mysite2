1d628f404a28bf9e53e84e41b212e044e836d30d
Jekyun Kim <kkyuni97@naver.com>
2023-07-27 10:19:28 +0900

    Email연동

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.googlemail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ejraks1997@gmail.com'
+EMAIL_HOST_PASSWORD = 'qclcpvovulpcfelu'
